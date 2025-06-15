aa3548593646773735d81461de77bdff06a4e144
Louis <louis.lacoste@agroparistech.fr>
2025-06-15 20:15:43 +0200

    Renaming and using dev script

--- dev/more_exploration.R
+++ dev/more_exploration.R
@@ -6,7 +6,7 @@ fit_iid <- readRDS(here("dev", "prefit_9_sim_iid.Rds"))
 netlist <- fit_iid$A
 names(netlist) <- fit_iid$net_id
 
-partition_init <- readRDS(here("dev", "partition.Rds"))
+# partition_init <- readRDS(here("dev", "partition.Rds"))
 
 # desc_res <- clusterize_bipartite_networks(
 #     netlist = netlist,
@@ -21,12 +21,12 @@ partition_init <- readRDS(here("dev", "partition.Rds"))
 # )
 library(future)
 options(future.globals.maxSize = 10 * 1024^3) # 100 MB
-plan("multisession", workers = 2L)
-res <- clusterize_bipartite_networks_d_a(
+plan("multisession", workers = 8L)
+res <- clusterize_bipartite_networks_more_exploration(
     netlist = netlist,
     net_id = names(netlist),
     colsbm_model = "iid",
-    full_collection_init = fit_iid,
+    # full_collection_init = fit_iid,
     # partition_init = partition_init,
     global_opts = list(
         nb_cores = 1L,
