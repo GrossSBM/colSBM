b44a3b886329dbaf479d139b510c0c218ede0466
Louis <louis.lacoste@agroparistech.fr>
2026-06-13 11:56:57 +0200

    Removing old comment

--- R/utils.R
+++ R/utils.R
@@ -431,7 +431,6 @@ spectral_biclustering <- function(A, Q, kmeans.nstart = 400L, kmeans.iter.max = 
   return(list(row_clustering = row_clustering, col_clustering = col_clustering))
 }
 
-# TODO : Modify the algorithm to use the rectangular matrix and its transpose
 bipartite_hierarchic_clustering <- function(X, K) {
   # Trivial clustering : everyone is part of the cluster
   if (all(K == c(1, 1))) {
