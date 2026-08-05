52b80627de42174d9c29d6094ef4620d0bbcdf76
Louis <louis.lacoste@agroparistech.fr>
2026-08-05 11:59:28 +0200

    Continue working on implementing the VE logic

--- src/colsbm_vem.cpp
+++ src/colsbm_vem.cpp
@@ -3,33 +3,58 @@
 #include <algorithm>
 #include <cmath>
 
-ColSBM::ColSBM(const std::vector<mat> &A_, int Q_) : A(A_), M((int)A_.size()), Q(Q_), iterations(0), vbound(0.0) {
+
+ColSBM::ColSBM(const std::vector<mat> &A_, int Q_, bool directed_, std::string distribution_, bool free_mixture_, bool free_density_)
+  : A(A_), M((int)A_.size()), Q(Q_), iterations(0), vbound(0.0), directed(directed_), free_mixture(free_mixture_), free_density(free_density_), distribution(std::move(distribution_)) {
   initialize_state();
 }
 
 ColSBM::~ColSBM() {}
 
 void ColSBM::initialize_state() {
+  mask.clear();
   tau.clear();
-  pi.clear();
+  pim.clear();
   emqr.clear();
   nmqr.clear();
-  alpha.clear();
+  alpham.clear();
   delta.clear();
+  vloss.clear();
+  logfactA.clear();
 
   for (int m = 0; m < M; ++m) {
+    // There we initialize the mask to mask the  diagonal at least
     const int n = A[m].n_rows;
+    mat mask_m = arma::zeros<mat>(n, n);
+    mask_m.diag().ones();
+    mask_m.elem(find_nonfinite(A[m])).ones();
+    mask.push_back(mask_m);
+
+    // The tau matrix is initialized with equal probability everywhere
     mat tau_m = arma::ones<mat>(n, Q) / static_cast<double>(Q);
     tau.push_back(tau_m);
 
+    // Equiprobability for pis
     rowvec pi_m(Q, arma::fill::value(1.0 / static_cast<double>(Q)));
-    pi.push_back(pi_m);
+    pim.push_back(pi_m);
 
     emqr.push_back(mat(Q, Q, arma::fill::zeros));
     nmqr.push_back(mat(Q, Q, arma::fill::zeros));
 
-    alpha.push_back(mat(Q, Q, arma::fill::value(0.5)));
-    delta.push_back(vec(1, arma::fill::value(1.0)));
+    alpham.push_back(mat(Q, Q, arma::fill::value(0.5)));
+    delta.push_back(1.0);
+
+    vloss.push_back(-arma::datum::inf);
+
+    double logfact = 0.0;
+    for (arma::uword i = 0; i < A[m].n_rows; ++i) {
+      for (arma::uword j = 0; j < A[m].n_cols; ++j) {
+        if (mask_m(i, j) < 0.5) {
+          logfact += std::lgamma(A[m](i, j) + 1.0);
+        }
+      }
+    }
+    logfactA.push_back(logfact);
   }
 
   vbound = 0.0;
@@ -40,9 +65,10 @@ void ColSBM::compute_aggregates() {
     const mat &X = A[m];
     const mat &tau_m = tau[m];
     const mat tau_t = tau_m.t();
+    const mat Um = 1.0 - mask[m];
 
-    mat emqr_m = tau_t * X * tau_m;
-    mat nmqr_m = tau_t * arma::ones<mat>(X.n_rows, X.n_cols) * tau_m;
+    mat emqr_m = tau_t * (X % Um) * tau_m;
+    mat nmqr_m = tau_t * Um * tau_m;
 
     emqr[m] = emqr_m;
     nmqr[m] = nmqr_m;
@@ -56,25 +82,121 @@ void ColSBM::update_pi() {
     for (int q = 0; q < Q; ++q) {
       pi_m[q] = arma::mean(tau_m.col(q));
     }
-    pi[m] = pi_m;
+    const double pi_sum = arma::accu(pi_m);
+    if (pi_sum > 0.0) {
+      pim[m] = pi_m / pi_sum;
+    } else {
+      pim[m] = rowvec(Q, arma::fill::value(1.0 / static_cast<double>(Q)));
+    }
   }
 }
 
-void ColSBM::step() {
-  compute_aggregates();
-  update_pi();
-
-  double sum_log = 0.0;
+void ColSBM::update_alpha() {
   for (int m = 0; m < M; ++m) {
-    const mat &em = emqr[m];
-    const mat &nm = nmqr[m];
-    for (int q = 0; q < Q; ++q) {
-      for (int r = 0; r < Q; ++r) {
-        sum_log += em(q, r) * clamp_log(std::max(em(q, r), 1e-10)) - nm(q, r) * 0.01;
+    const mat denom = arma::clamp(nmqr[m], TOL, arma::datum::inf);
+    mat alpha_m;
+
+    if (distribution == "bernoulli"){
+      alpha_m = clamp_matrix(emqr[m] / denom, TOL, 1.0);
+    } else {
+      alpha_m = clamp_matrix(emqr[m] / denom, TOL, arma::datum::inf);
+    }
+
+    if (!std::isfinite(arma::accu(alpha_m))) {
+      alpha_m.fill(0.5);
+    }
+    alpham[m] = alpha_m;
+  }
+}
+
+mat ColSBM::fixed_point_tau(int m, int max_iter, double tol) {
+  mat tau_old = tau[m];
+  const mat Um = 1.0 - mask[m];
+  const mat Am = A[m] % Um;
+
+  double prev_vloss = compute_network_vloss(m);
+  vloss[m] = prev_vloss;
+
+  for (int it = 0; it < max_iter; ++it) {
+    mat tau_new(tau_old.n_rows, tau_old.n_cols, arma::fill::zeros);
+    const mat log_pi = arma::repmat(log_clamped(pim[m]), tau_old.n_rows, 1);
+    const mat log_alpha = log_clamped(alpham[m] * delta[m]);
+    const mat log_1m_alpha = log_clamped(1.0 - alpham[m] * delta[m]);
+
+    mat score = log_pi + Am * tau_old * log_alpha.t() + Um * tau_old * log_1m_alpha.t();
+    if (directed) {
+      score += Am.t() * tau_old * log_alpha + Um.t() * tau_old * log_1m_alpha;
+    }
+
+    tau_new = softmax_rows(score);
+    tau[m] = tau_new;
+    compute_aggregates();
+    update_pi();
+
+    const double new_vloss = compute_network_vloss(m);
+    vloss[m] = new_vloss;
+
+    const double delta_tau = arma::accu(arma::square(tau_new - tau_old)) / static_cast<double>(tau_new.n_elem);
+    const double delta_vloss = std::abs(new_vloss - prev_vloss);
+    tau_old = tau_new;
+    prev_vloss = new_vloss;
+
+    if (delta_tau <= tol || delta_vloss <= tol) {
+      break;
+    }
+  }
+
+  return tau[m];
+}
+
+double ColSBM::compute_network_vloss(int m) const {
+  
+  const mat &tau_m = tau[m];
+  const mat &em = emqr[m];
+  const mat &nm = nmqr[m];
+  const double delta_m = delta[m];
+  const mat alpha_m = alpham[m] * delta_m;
+
+  // Vbound tau and alpha
+
+  // Vbound tau and pi
+  
+  // Vbound entropy
+  double obj = 0.0;
+  if (distribution == "bernoulli") {
+    obj += arma::accu(arma::clamp(em, 0.0, arma::datum::inf) % log_clamped(alpha_m));
+    obj += arma::accu(arma::clamp(nm - em, 0.0, arma::datum::inf) % log_clamped(1.0 - alpha_m));
+  } else {
+    obj += arma::accu(arma::clamp(em, 0.0, arma::datum::inf) % log_clamped(alpha_m));
+    obj -= arma::accu(nm % alpha_m);
+    obj -= logfactA[m];
+  }
+
+  const rowvec pi_m = pim[m];
+  for (arma::uword i = 0; i < tau_m.n_rows; ++i) {
+    for (arma::uword q = 0; q < tau_m.n_cols; ++q) {
+      const double val = tau_m(i, q);
+      if (val > 0.0) {
+        obj += val * clamp_log(pi_m(q));
+        obj -= val * clamp_log(val);
       }
     }
   }
-  vbound = sum_log;
+  return obj;
+}
+
+void ColSBM::step() {
+  for (int m = 0; m < M; ++m) {
+    fixed_point_tau(m, 1, 1e-6);
+  }
+  compute_aggregates();
+  update_pi();
+  update_alpha();
+
+  vbound = -arma::datum::inf;
+  for (int m = 0; m < M; ++m) {
+    vbound += compute_network_vloss(m);
+  }
   ++iterations;
 }
 
--- src/colsbm_vem.h
+++ src/colsbm_vem.h
@@ -9,27 +9,44 @@
 using arma::mat;
 using arma::vec;
 using arma::rowvec;
+using arma::uvec;
 
 class ColSBM {
 public:
   std::vector<mat> A; // adjacency matrices
+  std::vector<mat> mask;
   int M;
   int Q;
   int iterations;
   double vbound;
+  bool directed;
+  bool free_mixture;
+  bool free_density;
+  std::string distribution;
+  std::vector<double> logfactA;
   std::vector<mat> tau;
-  std::vector<rowvec> pi;
+  std::vector<rowvec> pim;
   std::vector<mat> emqr;
   std::vector<mat> nmqr;
-  std::vector<mat> alpha;
-  std::vector<vec> delta;
+  std::vector<mat> alpham;
+  std::vector<double> delta;
+  std::vector<double> vloss;
+  // TODO USE Will be used later to implement free mixture computation
+  // this is the support of size QxM indicating which block q 
+  // is populated by network m
+  std::vector<mat> Cpi;
+  std::vector<mat> Calpha;
 
-  ColSBM(const std::vector<mat> &A_, int Q_);
+
+  ColSBM(const std::vector<mat> &A_, int Q_, bool directed_ = false, std::string distribution_ = "bernoulli", bool free_mixture_ = true, bool free_density_ = true);
   ~ColSBM();
 
   void initialize_state();
   void compute_aggregates();
   void update_pi();
+  void update_alpha();
+  mat fixed_point_tau(int m, int max_iter = 1000, double tol = 1e-9);
+  double compute_network_vloss(int m) const;
   void step();
   void optimize(int max_step, double tol);
   double get_vbound() const;
